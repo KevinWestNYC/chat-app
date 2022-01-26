d1ce3bd32081b1fdf02f745a6969f059d5527052
Kevin West <kevinwestnyc@gmail.com>
2022-01-26 15:11:26 -0500

    functionality and some styling finished, want to add online list

--- server/index.js
+++ server/index.js
@@ -4,12 +4,33 @@ const http = require('http');
 const cors = require("cors");
 const PORT = 3001
 const { Server } = require("socket.io")
-
 app.use(cors())
 
 const server = http.createServer(app);
 
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        method: ['GET', 'POST'],
+    },
+})
 
+io.on("connection", (socket) => {
+    console.log(`User ${socket.id} connected!`);
+
+    socket.on("join_room", (data) => {
+        socket.join(data)
+        console.log(`User ${socket.id} joined room: ${data}`);
+    })
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    });
+});
 
 
 server.listen(PORT, () => {
